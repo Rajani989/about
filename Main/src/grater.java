f5b23d5638b06eaa78db3f5bc940277f61f77171
Rahul <Rahul@admin>
2022-08-16 14:44:02 +0530

    my first commit

---
+++ Main/src/grater.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class grater{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter 1st number: ");
+		int n1=sc.nextInt();
+		System.out.println("Enter 2nd number: ");
+		int n2=sc.nextInt();
+		System.out.println("Enter 3rd number: ");
+		int n3=sc.nextInt();
+		int n=(n1>n2)?(n1>n3?n1:n2):(n2>n3?n2:n3);
+		System.out.println(n);
+	}
+}
---
+++ Main/src/start.java
@@ -0,0 +1,60 @@
+import java.util.*;
+class start{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+	/*	int ang1=sc.nextInt();
+		int ang2=sc.nextInt();
+		int ang3=sc.nextInt();
+		if((ang1+ang2+ang3)==180) {
+			if((ang1 <90)&&(ang2<90)&&(ang3<90)) {
+				System.out.println("It's Acute angle triangle");
+			}
+			else if((ang1 ==90)||(ang2==90) ||(ang3==90))
+			{
+				System.out.println("It's right angled triangle.");
+			}
+			else {
+				System.out.println("It's obtuse angle triangle");
+			}
+		}
+		else {
+			System.out.println("It's not triangle");
+		}
+		
+		
+		
+		
+		
+		
+		System.out.println("Enter first subject marks: ");
+		int sub1=sc.nextInt();
+
+		System.out.println("Enter second subject marks: ");
+		int sub2=sc.nextInt();
+		
+		System.out.println("Enter third subject marks: ");
+		int sub3=sc.nextInt();
+		
+		int total=(sub1+sub2+sub3)/3;
+		
+		if(total>=70) {
+			System.out.println("Distinction");
+		}
+			else if(total>=60 && total<70) {
+			System.out.println("Ist class");
+		}
+			else if(total>=50 && total<60) {
+				System.out.println("2nd class");	
+			}
+			else if(total>=40 && total<50) {
+				System.out.println("3rd class");
+			}
+			else {
+				System.out.println("Fail");
+			}*/
+		
+		  
+		
+		
+		}
+	}
